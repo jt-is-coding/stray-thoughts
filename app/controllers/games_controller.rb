fe2a2d7436bdbf3a8d74bd70ce506e01d7d9787f
jeremybaxter <jrbaxter94@gmail.com>
2024-07-23 16:50:28 -0500

    added old code to games_controller search method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
   before_action :set_game, only: %i[ show edit update destroy ]
+  giant_bomb_key = ENV.fetch("GIANT_BOMB_KEY")
 
   # GET /games or /games.json
   def index
@@ -20,7 +21,37 @@ class GamesController < ApplicationController
   end
 
   def search
+
+    
+    @game_list = []
+    user_year = params.fetch("year")
+
+    if user_year.to_i > 2023 || user_year.to_i < 1972
+      user_year = rand(1972..2023)
+    end
+
+    giant_bomb_url1 = "https://www.giantbomb.com/api/games/?api_key=#{giant_bomb_key}&format=json&field_list=name,deck,image,platforms&filter=expected_release_year:#{user_year}"
+    raw_response = HTTP.get(giant_bomb_url1)
+    @parsed_response = JSON.parse(raw_response)
+    max_number = @parsed_response.fetch("number_of_total_results")
+    max_offset = max_number.to_i - 100
+    
+    if max_number > 100
+      giant_bomb_url2 = "https://www.giantbomb.com/api/games/?api_key=#{giant_bomb_key}&format=json&field_list=name,deck,image,platforms&offset=#{rand(100..max_offset)}&filter=expected_release_year:#{user_year}"
+      raw_response = HTTP.get(giant_bomb_url2)
+      @parsed_response = JSON.parse(raw_response)
+      @offset = @parsed_response.fetch("offset")
+      @results = @parsed_response.fetch("results")
+      @results.each do |number|
+        @game_list.push(number)
+      end
+    else
+      @results = @parsed_response.fetch("results")
+      @results.each do |number|
+      @game_list.push(number)
+    end
   end
+end
 
   # POST /games or /games.json
   def create
