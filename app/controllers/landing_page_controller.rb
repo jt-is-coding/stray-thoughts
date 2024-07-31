2951dca5da3ce2a5064a4776db107793753cec04
jeremybaxter <jrbaxter94@gmail.com>
2024-07-31 12:28:10 -0500

    working on landing page and background colors....

---
+++ app/controllers/landing_page_controller.rb
@@ -0,0 +1,6 @@
+class LandingPageController < ApplicationController
+
+  def home
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "users#home"
+  root "landing_page#home"
   devise_for :users
 
   resources :games do 
