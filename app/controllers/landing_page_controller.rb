1d97a59cad691dc4918f3cb46a85baf7cb9c150f
jeremybaxter <jrbaxter94@gmail.com>
2024-08-07 15:18:07 -0500

    Updated the landing page to display a specific note of mine for production

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,7 +1,7 @@
 class LandingPageController < ApplicationController
 
   def home
-    @note = Note.find_by(author_id: User.find_by(username: "jt"))
+    @note = Note.where({ author: User.find_by(username: "jt") }).at(2)
   end
 
 end
