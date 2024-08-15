ae6bc187f49a3a6f85d301ec5c6728612c39bb94
jeremybaxter <jrbaxter94@gmail.com>
2024-08-14 19:10:26 -0500

    Testing to see if this will grab the proper note for the landing page

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -2,7 +2,8 @@ class LandingPageController < ApplicationController
 
   def index
     authorize :landing_page, :index?
-    @note = Note.where({ author: User.find_by(username: "jt") }).at(2)
+    @note = Note.where({ parent: Game.find_by(api_id: "69675") }).first
+    #@note = Note.where({ author: User.find_by(username: "jt") }).sample
   end
 
 end
