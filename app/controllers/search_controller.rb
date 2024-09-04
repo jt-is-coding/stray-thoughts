8432f28bd2b5453237d5c24dc2fdfb5df02dbb91
Jeremy Baxter <37813767+jt-is-coding@users.noreply.github.com>
2024-09-04 19:14:57 +0000

    Made giant_bomb_service workable and implemented it in the search_controller. Changed the database production name, as well as added a conditional to note_small that makes it so that cards only hover within the notebook rather than everywhere.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,31 +8,8 @@ class SearchController < ApplicationController
 
     if @url.include?("Search") == true
       user_year = params.fetch(:year)
-      if user_year.to_i > 2023 || user_year.to_i < 1972
-        user_year = rand(1972..2023)
-      end
-      giant_bomb_url = "https://www.giantbomb.com/api/games/?api_key=#{ENV.fetch("GIANT_BOMB_KEY")}&format=json&field_list=id,name,deck,image,platforms&filter=expected_release_year:#{user_year}"
-      raw_response = HTTP.get(giant_bomb_url)
-      if raw_response != nil
-        @parsed_response = JSON.parse(raw_response)
-        max_number = @parsed_response.fetch("number_of_total_results")
-        max_offset = max_number.to_i - 100
-        if max_number > 100
-          giant_bomb_url = "https://www.giantbomb.com/api/games/?api_key=#{ENV.fetch("GIANT_BOMB_KEY")}&format=json&field_list=id,name,deck,image,platforms&offset=#{rand(100..max_offset)}&filter=expected_release_year:#{user_year}"
-          raw_response = HTTP.get(giant_bomb_url)
-          @parsed_response = JSON.parse(raw_response)
-          @offset = @parsed_response.fetch("offset")
-          @results = @parsed_response.fetch("results")
-          @results.each do |number|
-            @game_list.push(number)
-          end
-        else
-          @results = @parsed_response.fetch("results")
-          @results.each do |number|
-            @game_list.push(number)
-          end
-        end
-      end
+      giant_bomb_service = GiantBombService.new(user_year)
+      @game_list = giant_bomb_service.fetch_games
     end
   end
   
--- app/services/giant_bomb_service.rb
+++ app/services/giant_bomb_service.rb
@@ -1,27 +1,33 @@
 class GiantBombService
-  BASE_URL = "https://www.giantbomb.com/api/games"
+  def initialize(year)
+    @year = validate_year(year)
+    @api_key = ENV.fetch("GIANT_BOMB_KEY")
+    @base_url = "https://www.giantbomb.com/api/games/"
+  end
 
-  def call
-    url = BASE_URL + "?" + params.to_query
-    raw_response = HTTP.get(url)
-    parsed_response = JSON.parse(raw_response)
+  def fetch_games
+    initial_response = fetch_data(@year)
+    max_number = initial_response.fetch("number_of_total_results").to_i
+    if max_number > 100
+      offset = rand(100..(max_number - 100))
+      response = fetch_data(@year, offset)
+    else
+      response = initial_response
+    end
 
-    parsed_response.fetch("results")
+    response.fetch("results")
   end
 
   private
 
-  def params
-    {
-      api_key: api_key,
-      format: "json",
-      field_list: "id,name,deck,image,platforms",
-      # filter: "expected_release_year:#{year}",
-      # offset: rand(100..max_offset
-    }
+  def validate_year(year)
+    year.to_i.between?(1972, 2023) ? year : rand(1972..2023)
   end
 
-  def api_key
-    ENV.fetch("GIANT_BOMB_KEY")
+  def fetch_data(year, offset = nil)
+    url = "#{@base_url}?api_key=#{@api_key}&format=json&field_list=id,name,deck,image,platforms&filter=expected_release_year:#{year}"
+    url += "&offset=#{offset}" if offset
+    response = HTTP.get(url)
+    JSON.parse(response)
   end
 end
